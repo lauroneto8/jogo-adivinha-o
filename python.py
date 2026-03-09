77786050de1fda9ce2595485e3ac794705b52d6b
lauroneto8 <almeida.neto.lauro@escola.pr.gov.br>
2026-03-09 09:04:18 -0300

    python.py

--- python.py
+++ python.py
@@ -21,6 +21,8 @@ else (nivel == 3):
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    
+    
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
@@ -43,3 +45,6 @@ for rodada in range(1, total_de_tentativas + 1):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    Jogar()
